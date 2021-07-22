c68fbe2130c6689e753695675c22dfde61fb200a
Hassan <hassanirfan429@gmail.com>
2021-07-22 13:31:25 +0500

    spread work

---
+++ app.js
@@ -0,0 +1,16 @@
+// function sum(x, y, z , a , b) {
+//     return x + y + z + a + b;
+//   }
+  
+//   const food = ['TIKKA', ' ','BEEFPULAO',' ', 'PAYA' ];
+  
+//   console.log(sum(...food));
+  
+  
+  //console.log(sum.apply(null, food));
+  
+ 
+  const intialNumber = [1 , 2 , 3]
+  const finalNumber = [...intialNumber, 4 , 5 , 6]
+
+  console.log(finalNumber);
